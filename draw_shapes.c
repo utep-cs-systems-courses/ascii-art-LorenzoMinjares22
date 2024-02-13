732de87278c32233f23beebbe2d432bb9c85573c
Lorenzo Minjares <lminjares3@miners.utep.edu>
2024-02-13 12:48:54 -0700

    finished lab 0

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,10 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+//prints arrow
+
+void print_arrow(int leftCol, int size)
+{
+  print_triangle(leftCol,size);
+  print_square(leftCol+(leftCol-1),size);
+}
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = a, Square = s, Chars 11x16= c, \nChars 5x7 = z,  Arrow = t) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -17,6 +17,10 @@ int main()
     // Given the user answer, select which method to call
     switch (c) {
     case 't':
+      puts("You selected arrow");
+      print_arrow(5,7);
+      break;
+    case 'a':
       puts("You selected triangle:");
       print_triangle(5, 7);
       break;
@@ -25,9 +29,14 @@ int main()
       print_square(5, 5);
       break;
     case 'c':
-      puts("You selected chars:");
+      puts("You selected chars font 11x16:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case 'z':
+      puts("you slected chars font 5x7:");
+      for (char c ='A'; c<'D'; c++)
+	print_char_5x7(c);
       break;
     case 'q':
       puts("Bye!");
